f5a523a050455379b17880ccc0a00cf03c617c67
yumichoi <yumichoi0921@gmail.com>
2021-04-02 02:51:53 +0900

    답변 등록 기능 구현

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question
+from django.utils import timezone
+
 # Create your views here.
 
 
@@ -25,3 +27,16 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    """
+    pybo 답변 등록
+    :param request:
+    :param question_id:
+    :return:
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
+
