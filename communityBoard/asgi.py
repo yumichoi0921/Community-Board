9ad88e5119a48b92bcccc3281fedce767fc9b9cc
yumichoi <yumichoi0921@gmail.com>
2021-04-03 03:24:42 +0900

    bootstrap & css 적용 표준 HTML & 템플릿 상속 사용

--- communityBoard/asgi.py
+++ communityBoard/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for communityBoard project.
+ASGI communityBoard for communityBoard project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- communityBoard/wsgi.py
+++ communityBoard/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for communityBoard project.
+WSGI communityBoard for communityBoard project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
